c1338f5b938788485fdf0e49fd8d45c1b8aa08d2
Florian Völker <Neirolf2030@web.de>
2018-10-14 16:12:26 +0200

    initial commit for package

---
+++ complex.ts
@@ -0,0 +1,77 @@
+class Complex {
+    re: number;
+    im: number;
+    constructor(re, im) {
+        this.re = re || 0;
+        this.im = im || 0;
+    }
+
+    /**
+     * Multiplies the complex numbers a and b.
+     */
+    static multiply = function multiply(a: Complex, b: Complex): Complex {
+        return new Complex(a.re * b.re - a.im * b.im,
+            a.re * b.im + b.re * a.im);
+    }
+
+    /**
+     * Adds Complex a to Complex b
+     */
+    static add = function add(a: Complex, b: Complex): Complex {
+        return new Complex(a.re + b.re, a.im + b.im);
+    }
+
+    /**
+     * Divide a complex number by an real number
+     * 
+     * n / c
+     * 
+     * divideNCompl(3, new Complex(2, -5)) == 3 / (2 - 5i)
+     */
+    static divideNCompl = function divideNCompl(n: number, c: Complex): Complex {
+        const nDivByk = n / (c.re * c.re + c.im * c.im);
+
+        return new Complex(c.re * nDivByk, -c.im * nDivByk);
+    }
+
+    /**
+     * Calculates the complex nth-roots of the given complex number.
+     */
+    static root = function root(c: Complex, n: number): Complex[] {
+        const a = c.re;
+        const b = c.im;
+
+        // length of the complex number 
+        const r = Math.sqrt(a * a + b * b);
+
+        const phi = Math.acos(a / r);
+
+        const roots = new Array(n);
+        const nthRootOfR1 = Math.pow(r, 1 / n);
+        const nthRootOfR2 = (b < 0 ? -nthRootOfR1 : nthRootOfR1);
+        for (let k = 0; k < n; k++) {
+            const d = (k * 2 * Math.PI + phi) / n;
+            roots[k] = new Complex(nthRootOfR1 * Math.cos(d), nthRootOfR2 * Math.sin(d));
+        }
+
+        return roots;
+    }
+
+    /**
+     * Is a simplified variant of the root(), which is somewhat faster and only returns the first root.
+     */
+    static cubicRoot = function cubicRoot(c: Complex): Complex {
+        const a = c.re;
+        const b = c.im;
+
+        // length of the complex number 
+        const r = Math.sqrt(a * a + b * b);
+
+        const nthRootOfR1 = Math.pow(r, 1 / 3);
+        const nthRootOfR2 = (b < 0 ? -nthRootOfR1 : nthRootOfR1);
+        const d = Math.acos(a / r) / 3;
+        return new Complex(nthRootOfR1 * Math.cos(d), nthRootOfR2 * Math.sin(d));
+    }
+}
+
+export default Complex;
---
+++ index.ts
@@ -0,0 +1,114 @@
+import Complex from './complex.js';
+
+/**
+ * Solves 0 = a*x + b for x
+ */
+function solveLinearEquation(a: number, b: number) {
+    if (a == 0)
+        if (b != 0)
+            return [];
+        else
+            return [0];
+    else
+        return [-b / a];
+}
+
+/**
+ * Solves 0 = a*x^2 + b*x + c for x
+ */
+function solveQuadraticEquation(a: number, b: number, c: number) {
+    if (a == 0)
+        return solveLinearEquation(b, c);
+
+    const p = b / a;
+    const q = c / a;
+
+    const sqrtContent = p / 2 * p / 2 - q;
+    if (sqrtContent < 0) {
+        const sqrtR = Math.sqrt(-sqrtContent);
+
+        return [new Complex(-p / 2, sqrtR), new Complex(-p / 2, -sqrtR)];
+    } else {
+        const sqrtR = Math.sqrt(sqrtContent);
+
+        return [-p / 2 + sqrtR, -p / 2 - sqrtR];
+    }
+}
+
+const c1Multi = new Complex(-0.5, 0.5 * Math.sqrt(3));
+const c2Multi = new Complex(-0.5, -0.5 * Math.sqrt(3));
+
+function calcC(d0: number, d1: number): Complex {
+    const sqrtContent = d1 * d1 - 4 * d0 * d0 * d0;
+    if (sqrtContent < 0) {
+        const imaginaryPart = Math.sqrt(-sqrtContent) / 2;
+        const realPart = d1 / 2;
+
+        // you could also use Complex.root to get all roots, to prevent the usage of 
+        // multiplications for c1 and c2 in solveCubicEquation
+        // const [c0,c1,c2] = Complex.root(new Complex(realPart, imaginaryPart), 3);
+
+        return Complex.cubicRoot(new Complex(realPart, imaginaryPart));
+    } else {
+        return new Complex(Math.cbrt((d1 + Math.sqrt(sqrtContent)) / 2), 0);
+    }
+}
+
+function calcX(d0: number, c: Complex, a: number, b: number): Complex {
+    // -1 / (3 * a) * (b + c + d / c)
+    const dDivc = Complex.divideNCompl(d0, c);
+    const res = new Complex(-(dDivc.re + c.re + b) / (3 * a), -(dDivc.im + c.im) / (3 * a));
+    return res;
+}
+
+/**
+ * Solves 0 = a*x^3 + b*x^2 + c*x + d for x
+ */
+function solveCubicEquation(a: number, b: number, c: number, d: number) {
+    if (a == 0)
+        return solveQuadraticEquation(b, c, d);
+
+    if (d == 0) {
+        const x = solveQuadraticEquation(a, b, c);
+        const y: (number | Complex | undefined)[] = [0];
+        const l = x.length;
+
+        if (l > 0) {
+            y[1] = x[0];
+            if (l > 1) {
+                y[2] = x[1];
+            }
+        }
+
+        return y;
+    }
+
+    const delta0 = b * b -
+        3 * a * c;
+
+    const delta1 = 2 * b * b * b -
+        9 * a * b * c +
+        27 * a * a * d;
+
+    // const delta = 18 * a * b * c * d -
+    //     4 * b * b * b * d +
+    //     b * b * c * c -
+    //     4 * a * c * c * c -
+    //     27 * a * a * d * d;
+
+    const c0 = calcC(delta0, delta1);
+    const c1 = Complex.multiply(c0, c1Multi);
+    const c2 = Complex.multiply(c0, c2Multi);
+
+    const x0 = calcX(delta0, c0, a, b);
+    const x1 = calcX(delta0, c1, a, b);
+    const x2 = calcX(delta0, c2, a, b);
+
+    return [x0, x1, x2];
+}
+
+export {
+    solveLinearEquation,
+    solveQuadraticEquation,
+    solveCubicEquation
+}
